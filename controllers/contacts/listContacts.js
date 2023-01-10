64fd55e1289504508cb66e5294f295cec2836cd5
Alona Beskhlibnova <a.beskhlibnova@gmail.com>
2023-01-10 16:59:57 +0200

    fix auth

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -4,9 +4,9 @@ const listContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
-  const query = { owner };
-  query.favorite = favorite ? favorite : { $in: [true, false] };
-  const result = await Contact.find(query, "name email phone", {
+  // const query = { owner };
+  // query.favorite = favorite ? favorite : { $in: [true, false] };
+  const result = await Contact.find({ owner }, "name email phone", {
     skip,
     limit,
   }).populate("owner", "email");
